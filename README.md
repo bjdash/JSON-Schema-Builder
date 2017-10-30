7bcfd2592f569f748b61e59818777b6c6b5652d2
Bijay <bijaya.dash@honeywell.com>
2017-10-30 18:44:23 +0530

    Updated docs

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # JSON Schema Builder
 A simple GUI tool to enable designing and building JSON schemas
 
-## Example
-You can see a working version of this at [JSON Schema Builder](https://bjdash.github.io/JSON-Schema-Builder/)
+## DOcs & Example
+You can see the documentation and a working version of this at [JSON Schema Builder](https://bjdash.github.io/JSON-Schema-Builder/). 
+This library was developed for [APIC](http://myapic.com) and has been extensively used in the [Chrome APP](https://chrome.google.com/webstore/detail/apic-complete-api-solutio/ggnhohnkfcpcanfekomdkjffnfcjnjam).
--- docs/js/app.js
+++ docs/js/app.js
@@ -5,8 +5,8 @@ angular.module('schema-builder', ['ui.bootstrap', 'json-schema']);
 angular.module('schema-builder')
 .controller('schemaCtrler', schemaCtrler);
 
-schemaCtrler.$inject = ['$scope'];
-function schemaCtrler($scope) {
+schemaCtrler.$inject = ['$scope', 'JsonSchema'];
+function schemaCtrler($scope, JsonSchema) {
     $scope.refModels = {
         "1496757761729": {
             "name": "User",
@@ -29,6 +29,25 @@ function schemaCtrler($scope) {
         }
     };
 
-    //close model selector custom dropdown dropdown
+    var schema = {
+        "type": "object",
+        "properties": {
+            "name": {
+                "type": "string",
+                "minLength": 3,
+                "maxLength": 255
+            },
+            "age": {
+                "type": "integer",
+                "minimum": 18
+            }
+        },
+        "required": [
+            "name",
+            "age"
+        ]
+    }
     
+    $scope.data = JsonSchema.schema2obj(schema);
+    $scope.data.root$$ = true;
 }
