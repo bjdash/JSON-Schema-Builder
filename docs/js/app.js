e534347135c725997d5b4ad49554a440f4c6323b
Bijay <bijaya.dash@honeywell.com>
2017-10-30 16:30:34 +0530

    Support for $ref, multiple schema type, exposed service

--- docs/js/app.js
+++ docs/js/app.js
@@ -3,9 +3,32 @@
 angular.module('schema-builder', ['ui.bootstrap', 'json-schema']);
 
 angular.module('schema-builder')
-        .controller('schemaCtrler', schemaCtrler);
+.controller('schemaCtrler', schemaCtrler);
 
 schemaCtrler.$inject = ['$scope'];
-function schemaCtrler($scope){
+function schemaCtrler($scope) {
+    $scope.refModels = {
+        "1496757761729": {
+            "name": "User",
+            "nameSpace": "user",
+            "data": {
+                "type": ["object"],
+                "properties": {
+                    "name": {
+                        "type": ["string"]
+                    }
+                }
+            }
+        },
+        "1497245758395": {
+            "name": "Address",
+            "nameSpace": "address",
+            "data": {
+                "type": ["object"]
+            }
+        }
+    };
+
+    //close model selector custom dropdown dropdown
     
 }
